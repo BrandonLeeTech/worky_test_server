2d05fb2977cf178801fa0fd139df9e36e4c877d9
brandon.lee <brandon.lee@gamehours.com>
2025-05-09 09:48:27 +0800

    調整 import 路徑

--- backend/web/__init__.py
+++ backend/web/__init__.py
@@ -1,5 +1,5 @@
 """ 後台操作 """
 
-from backend.web.shop_approve import shop_audit_passed
+from web.shop_approve import shop_audit_passed
 from web.set_credit_card import credit_card_bind
 from web.labor_identity_verification import labor_verify
